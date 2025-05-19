46973f110d8b383bd1235fb62c47bc6d89609d6c
Luiza Thier <lu.thier02@gmail.com>
2025-05-19 09:27:43 -0300

    interaçaotabela

--- script.js
+++ script.js
@@ -8,4 +8,26 @@ colecaoElementos.forEach(el => {
   div.style.backgroundColor = el.corGrupo || '#ccc';
   div.innerHTML = `<strong>${el.simbolo}</strong><br>${el.numeroAtomico}`;
   container.appendChild(div);
+
+
+  
+  div.addEventListener('mousemove', (e) => {
+    minecraft.style.display = 'block';
+    minecraft.style.left = e.pageX + 15 + 'px';
+    minecraft.style.top = e.pageY + 15 + 'px';
+    minecraft.innerHTML = `
+      <strong>${el.nome} (${el.simbolo})</strong><br>
+      Número Atômico: ${el.numeroAtomico}<br>
+      Massa Atômica: ${el.massaAtomica}<br>
+      Eletronegatividade: ${el.eletronegatividade || 'N/A'}<br>
+      Ponto de Fusão: ${el.pontoDeFusao || 'N/A'} K<br>
+      Estado: ${el.estadoPadrao}<br>
+      Grupo: ${el.grupo}
+    `;
+  });
+ 
+  div.addEventListener('mouseleave', () => {
+    minecraft.style.display = 'none';
+  });
 });
+const minecraft = document.getElementById('minecraft');
